1b14ce2e3014263a5ef3965f4feb9d710d39d2a1
Shashikant <yadavshashikant43@gmail.com>
2021-10-30 22:08:59 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar()
     return(
         <div className='navbar'>
             <h1>NonStandard</h1>
-            <h3>One place for alld nonstandard Characters</h3>
+            <h3>One place for all nonstandard Characters</h3>
         </div>
     );
 }
